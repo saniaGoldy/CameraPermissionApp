49a13b35b4fab25ee473517dc55e6e5340fa6bc6
Maksym Motornyi <maksym_motornyi@epam.com>
2020-04-29 23:03:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/stuff/CameraActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication.stuff
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
+
+abstract class CameraActivity : AppCompatActivity() {
+
+    abstract var previewView: PreviewView
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+    }
+
+    protected fun launchCamera() {
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val preview: Preview = Preview.Builder()
+            .build()
+
+        val cameraSelector: CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        val camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+
+        preview.setSurfaceProvider(previewView.createSurfaceProvider(camera.cameraInfo))
+    }
+}
